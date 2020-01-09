7367bc2cdd19359dd8b12fc8eea768326b4ed4ab
jgarbalski <jakub.garbalski@bluesoft.com>
2020-01-09 13:16:29 +0100

    FIRST WORKING VERSION IF ANYTHING GOES WRONG ROLLBACK HERE uploaded files are saved to /uploads folder

--- app.py
+++ app.py
@@ -1,13 +1,33 @@
-from flask import Flask, render_template      
+import os
+
+from flask import Flask, render_template, request
+from flask_dropzone import Dropzone
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 
 app = Flask(__name__)
 
+app.config.update(
+    UPLOADED_PATH=os.path.join(basedir, 'uploads'),
+    # Flask-Dropzone config:
+    #DROPZONE_ALLOWED_FILE_TYPE='music',
+    DROPZONE_MAX_FILE_SIZE=20,
+    DROPZONE_MAX_FILES=1,
+    DROPZONE_UPLOAD_ON_CLICK=True
+)
 
-@app.route("/")
-def home():
-    return render_template("index.html")
+dropzone = Dropzone(app)
 
-    
-if __name__ == "__main__":
+
+@app.route('/', methods=['POST', 'GET'])
+def upload():
+    if request.method == 'POST':
+        for key, f in request.files.items():
+            if key.startswith('file'):
+                f.save(os.path.join(app.config['UPLOADED_PATH'], f.filename))
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
     app.run(debug=True)
-  
