00387acff28745fb3fc8933560afb2a7775ca578
jgarbalski <jakub.garbalski@bluesoft.com>
2020-01-09 13:31:08 +0100

    PIERWSZA DZIALAJACA WERSJA W RAZIE COS SIE SPIERDOLI TO COFNAC DO TEGO PUNKTU

--- app.py
+++ app.py
@@ -1,12 +1,33 @@
-from flask import Flask, escape, request, render_template
+import os
+
+from flask import Flask, render_template, request
+from flask_dropzone import Dropzone
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 
 app = Flask(__name__)
 
+app.config.update(
+    UPLOADED_PATH=os.path.join(basedir, 'uploads'),
+    # Flask-Dropzone config:
+    #DROPZONE_ALLOWED_FILE_TYPE='music',
+    DROPZONE_MAX_FILE_SIZE=20,
+    DROPZONE_MAX_FILES=1,
+    DROPZONE_UPLOAD_ON_CLICK=True
+)
 
-@app.route('/')
-def main():
-    return render_template("front/index.html")
+dropzone = Dropzone(app)
 
 
-if __name__ == "__main__":
-    app.run(port=5002)
+@app.route('/', methods=['POST', 'GET'])
+def upload():
+    if request.method == 'POST':
+        for key, f in request.files.items():
+            if key.startswith('file'):
+                f.save(os.path.join(app.config['UPLOADED_PATH'], f.filename))
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
