0a57cb5061f8aa706dccebbfc70b5163204a3496
moksmok <marcelzdrojewski@mMBP-elo.local>
2020-01-14 01:04:45 +0100

    Modified css, added fonts and colors

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
 from flask_dropzone import Dropzone
 
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -17,20 +17,50 @@ app.config.update(
     DROPZONE_UPLOAD_ON_CLICK=True
 )
 
-dropzone = Dropzone(app)
 
+spleeter_command = ""
 
 @app.route('/', methods=['POST', 'GET'])
 def upload():
     music_type = request.form.get("type")
 
+
+    def stems(x):
+        return {
+            'vocals': "2stems",
+            'instrumental': "3stems",
+            'vocals-drums': "4stems",
+            'drums': "5stems",
+        }[x]
+        
     if request.method == 'POST':
         print(music_type)
-        
-        # for key, f in request.files.items():
-        #     if key.startswith('file'):
-        #         f.save(os.path.join(app.config['UPLOADED_PATH'], f.filename))
+        for key, f in request.files.items():
+            if key.startswith('file'):
+                f.save(os.path.join(app.config['UPLOADED_PATH'], f.filename))
+                global spleeter_command
+                spleeter_command = str("spleeter separate -i " + str(os.path.join(app.config['UPLOADED_PATH'], f.filename)) + " -o audio_output -p spleeter:" + stems(music_type) + "-16kHz")
+                # run_spleeter()
     return render_template('index.html')
+
+
+
+
+@app.route('/spleeter')
+def run_spleeter():
+    print("IDZIE DALEJ")
+    print(spleeter_command)
+    return spleeter_command
+    
+    
+
+
+# @app.route("/forward/", methods=['POST'])
+# def move_forward():
+#     if request.method == 'POST':
+#     #Moving forward code
+#         print("idzie")
+#         return render_template('index.html')
 
 
 # @app.route('/register', methods=['GET', 'POST'])
--- static/js/main.js
+++ static/js/main.js
@@ -16,33 +16,24 @@
 // });
 
 
+
+
 $(document).ready(function(){
     $('form input').change(function () {
       $('form p').text(this.files.length + " file(s) selected");
     });
 
-    $('form').on('submit', function(event) {
+    $('input').click(function(){
+        document.getElementById("successMsg").style.display="none";
+        document.getElementById("submit-btn").style.display="block";
+    });
+
+    $('#form-send').on('submit', function(event) {
         event.preventDefault();
 
-        var formData = new FormData($('form')[0]);
+        var formData = new FormData($('#form-send')[0]);
 
         $.ajax({
-
-            xhr : function() {
-				var xhr = new window.XMLHttpRequest();
-
-				xhr.upload.addEventListener('progress', function(e) {
-
-					if (e.lengthComputable) {
-						console.log('Bytes Loaded: ' + e.loaded);
-						console.log('Total Size: ' + e.total);
-						console.log('Percentage Uploaded: ' + (e.loaded / e.total))
-					}
-				});
-                return xhr;
-            },
-
-
             type : 'POST',
             url : '/',
             data : formData,
@@ -51,37 +42,43 @@ $(document).ready(function(){
             success : function() {
                 // alert('File uploaded!');
                 document.getElementById("successMsg").style.display="block";
+                document.getElementById("submit-btn").style.display="none";
             }
         })
-    })
-  });
+    });
 
 
+    $("#test-btn").click(function (event) { 
+        $.getJSON('/spleeter',
+            function(data) { 
+                print("mleko")
+            });
+        return 'OK'; });
+})
 
 
+//     $('#form-forward').on('submit', function(event) {
+//         event.preventDefault();
 
+//         var formData1 = new FormData($('#form-forward')[0]);
 
-// $("#upload-file").submit(function (event) {
-//     event.preventDefault();
-//     //grab all form data  
-//     var formData = $(this).serialize();
-
-//  $.ajax({
-//         url: '',
-//         type: 'POST',
-//         data: formData,
-//         async: false,
-//         cache: false,
-//         contentType: false,
-//         processData: false,
-//         success: function (returndata) {
-//         $("#productFormOutput").html(returndata);
-//         alert(formData);
-//         },
-//         error: function(){
-//             alert("error in ajax form submission");
+//         $.ajax({
+//             type : 'POST',
+//             url: "/spleeter",
+//             data : formData1,
+//             processData : false,
+//             contentType : false,
+//             success: function(result){
+//                 console.log(result);
 //             }
-//     });
+            
+//             });
+//       });
+//   });
 
-//     return false;
-//     });
+  
+
+
+
+
+
