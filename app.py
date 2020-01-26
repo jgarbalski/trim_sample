15a9d543c30ebf525daaec910c9618c44b6c57ec
Kuba Garbalski <kuba.garbalski@op.pl>
2020-01-26 14:09:34 +0100

    changed app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request
 from flask_dropzone import Dropzone
 
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -11,20 +11,21 @@ app = Flask(__name__)
 app.config.update(
     UPLOADED_PATH=os.path.join(basedir, 'uploads'),
     # Flask-Dropzone config:
-    #DROPZONE_ALLOWED_FILE_TYPE='music',
+    DROPZONE_ALLOWED_FILE_TYPE='audio',
     DROPZONE_MAX_FILE_SIZE=20,
     DROPZONE_MAX_FILES=1,
     DROPZONE_UPLOAD_ON_CLICK=True
 )
 
+dropzone = Dropzone(app)
 
 spleeter_command = ""
 
+
 @app.route('/', methods=['POST', 'GET'])
 def upload():
-    music_type = request.form.get("type")
-    
-    
+
+    music_type = request.form.get('type')
 
     def stems(x):
         return {
@@ -33,49 +34,21 @@ def upload():
             'vocals-drums': "4stems",
             'drums': "5stems",
         }[x]
-        
+
     if request.method == 'POST':
-        print(music_type)
         for key, f in request.files.items():
             if key.startswith('file'):
                 f.save(os.path.join(app.config['UPLOADED_PATH'], f.filename))
                 global spleeter_command
-                spleeter_command = str("spleeter separate -i " + str(os.path.join(app.config['UPLOADED_PATH'], f.filename)) + " -o audio_output -p spleeter:" + stems(music_type) + "-16kHz")
-                # run_spleeter()
+                spleeter_command = str("spleeter separate -i \"" + str(os.path.join(app.config['UPLOADED_PATH'], f.filename)) + "\" -o audio_output -p spleeter:" + stems(music_type) + "-16kHz")
     return render_template('index.html')
 
 
-
-
-@app.route('/spleeter',  methods=['POST', 'GET'])
+@app.route('/spleeter')
 def run_spleeter():
-    print("IDZIE DALEJ")
+    print(spleeter_command)
     return spleeter_command
 
-
-@app.route('/submit', methods=['POST', 'GET'])
-def submit_file():
-    print("wysyla plik")
-    
-    
-    
-    
-
-
-# @app.route("/forward/", methods=['POST'])
-# def move_forward():
-#     if request.method == 'POST':
-#     #Moving forward code
-#         print("idzie")
-#         return render_template('index.html')
-
-
-# @app.route('/register', methods=['GET', 'POST'])
-# def register():
-#     music_type = request.form.get("audio-type")
-
-#     if request.method == "POST":
-#         print(music_type)
 
 if __name__ == '__main__':
     app.run(debug=True)
